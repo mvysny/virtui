3cf33ee1eb6f1c4ed5ae25a73ee0568e704c1650
Martin Vysny <mavi@vaadin.com>
2025-11-05 13:23:54 +0200

    formatting

--- lib/lazyvirt.rb
+++ lib/lazyvirt.rb
@@ -10,40 +10,47 @@ require 'io/console'
 scheduler = Rufus::Scheduler.new
 
 virt = VirtCmd.new
-#virt = LibVirtClient.new
+# virt = LibVirtClient.new
 virt_cache = VirtCache.new(virt)
 
 class Formatter
   def initialize
     @p = Pastel.new
   end
+
   def format(what)
     return format_cpu(what) if what.is_a? CpuInfo
     return format_memory_stat(what) if what.is_a? MemoryStat
     return format_mem_stat(what) if what.is_a? MemStat
     return format_memory_usage(what) if what.is_a? MemoryUsage
+
     what.to_s
   end
+
   # @param cpu [CpuInfo]
   def format_cpu(cpu)
     "#{@p.bright_blue('CPU')}: #{@p.bright_blue(cpu.model)}: #{@p.cyan(cpu.cpus)}:#{cpu.sockets}/#{cpu.cores_per_socket}/#{cpu.threads_per_core} sockets/cores/threads"
   end
+
   # @param ms [MemoryStat]
   def format_memory_stat(ms)
     "#{@p.bright_red('RAM')}: #{format(ms.ram)}; #{@p.bright_red('SWAP')}: #{format(ms.swap)}"
   end
+
   # @param mu [MemoryUsage]
   def format_memory_usage(mu)
     "#{@p.cyan(format_byte_size(mu.used))}/#{@p.cyan(format_byte_size(mu.total))} (#{@p.cyan(mu.percent_used)}%)"
   end
+
   # @param state [Symbol] one of `:running`, `:shut_off`, `:paused`
   def format_domain_state(state)
-    state = case state
-      when :running; @p.green('running')
-      when :shut_off; @p.red('shut_off')
-      else; @p.yellow(state)
+    case state
+    when :running then @p.green('running')
+    when :shut_off then @p.red('shut_off')
+    else; @p.yellow(state)
     end
   end
+
   # @param ms [MemStat]
   def format_mem_stat(ms)
     result = "Host:#{format(ms.host_mem)}"
@@ -61,7 +68,7 @@ class SystemWindow < Window
     @cpu = @f.format(virt_cache.cpu_info)
     update
   end
-  
+
   def update
     content do |lines|
       lines << @cpu
@@ -78,9 +85,9 @@ class VMWindow < Window
     @virt_cache = virt_cache
     update
   end
-  
+
   def update
-    domains = @virt_cache.domains.sort_by(&:name)    # Array<DomainId>
+    domains = @virt_cache.domains.sort_by(&:name) # Array<DomainId>
     content do |lines|
       domains.each do |domain_id|
         line = $p.white(domain_id.name)
@@ -102,22 +109,22 @@ class Screen
     @system = SystemWindow.new(virt_cache)
     @vms = VMWindow.new(virt_cache)
   end
-  
+
   # Clears the TTY screen
   def clear
     print TTY::Cursor.move_to(0, 0), TTY::Cursor.clear_screen
   end
-  
+
   # Re-calculates all window sizes and re-positions them. Call initially, and
   # when TTY size changes.
   def calculate_window_sizes
     clear
-    sh, sw = TTY::Screen.size
+    _, sw = TTY::Screen.size
     left_pane_w = sw / 2
     @system.rect = Rect.new(0, 0, left_pane_w, 4)
     @vms.rect = Rect.new(0, 4, left_pane_w, 10)
   end
-  
+
   def update_data
     @system.update
     @vms.update
@@ -128,7 +135,7 @@ screen = Screen.new(virt_cache)
 screen.calculate_window_sizes
 
 # Trap the WINCH signal (sent on terminal resize)
-trap("WINCH") do
+trap('WINCH') do
   screen.calculate_window_sizes
 end
 
