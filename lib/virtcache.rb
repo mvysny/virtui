6f990a5b1120b16d477cc14cf98c27e00e4d6743
Martin Vysny <mavi@vaadin.com>
2025-11-06 00:00:47 +0200

    virtcache optimization

--- lib/virtcache.rb
+++ lib/virtcache.rb
@@ -24,42 +24,32 @@ class VirtCache
 
   # @return [Set<DomainId>]
   def domains
-    @domains.keys
+    @domain_data.keys
   end
 
   # @param domain [DomainId]
   # @return [MemStat | nil] nil if domain isn't running
   def memstat(domain)
-    @mem_stats[domain]
+    @domain_data[domain].mem_stat
   end
 
   # @param domain [DomainId]
   # @return [Symbol] one of `:running`, `:shut_off`, `:paused`, `:other`
   def state(domain)
-    @domains[domain] || :other
+    @domain_data[domain].info.state || :other
   end
 
   # Updates the cache
-  def update_slow
-    domains = @virt.domains
-    @domains = domains.map { |d| [d.id, d.state] }.to_h
-    @mem_stats = @domains.keys.map { |id| [id, id.running? ? @virt.memstat(id) : nil] }.to_h
-    @host_mem_stat = @sysinfo.memory_stats
-    @host_cpu_usage = @sysinfo.cpu_usage(@host_cpu_usage)
-  end
-  
   def update
-    domain_data = @virt.domain_data
-    domain_data = domain_data.map { |domain_name, data| [DomainId.new(data.running? ? domain_name.hash : nil, domain_name), data] }.to_h 
-    @domains = domain_data.map { |did, data| [did, data.info.state] }.to_h
-    @mem_stats = @domains.keys.map { |id| [id, id.running? ? domain_data[id].mem_stat : nil] }.to_h
+    @domain_data = @virt.domain_data
+    @domain_data = @domain_data.map { |domain_name, data| [DomainId.new(data.running? ? domain_name.hash : nil, domain_name), data] }.to_h
     @host_mem_stat = @sysinfo.memory_stats
     @host_cpu_usage = @sysinfo.cpu_usage(@host_cpu_usage)
   end
 
   # @return [Integer] a sum of RSS usage of all running VMs
   def total_vm_rss_usage
-    @mem_stats.values.sum { |mem_stat| mem_stat&.rss || 0 }
+    @domain_data.values.sum { |data| data.mem_stat.rss || 0 }
   end
 
   # @return [Float] recent CPU usage, 0..100%
