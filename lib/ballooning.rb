1f2679d9ebe767f730269e3f43de861db6529c29
Martin Vysny <mavi@vaadin.com>
2025-11-13 10:25:59 +0200

    minor

--- lib/ballooning.rb
+++ lib/ballooning.rb
@@ -24,14 +24,22 @@ class BallooningVM
   def initialize(virt_cache, vmid)
     @virt_cache = virt_cache
     @vmid = vmid
+    # don't go below 1GB, or above max_memory
+    @min_active = 1 * 1024 * 1024 * 1024
+    # After Ballooning decreases active memory, it will back off for 20 seconds
+    # before trying to decrease the memory again. Observation shows that
+    # the effects of the memory decrease command in Linux guest isn't instant: instead it is gradual, and takes
+    # some time (~15 second) to fully be applied. Let's not bother the VM with further
+    # memory decrease commands until the VM fully settles in.
+    @back_off_seconds = 20
   end
 
   # Call every 2 seconds, to control the VM
   def update
     mem_stat = @virt_cache.memstat(@vmid)
     if !@virt_cache.running?(@vmid) || mem_stat.nil?
-      # VM is shut off. mark as back_off - we don't want to downgrade the memory
-      # while the VM is booting up.
+      # VM is shut off. Don't fiddle with the memory.
+      # Mark as back_off - this way we'll back off from the VM until it boots up.
       back_off
       return
     end
@@ -62,11 +70,9 @@ class BallooningVM
     # calculate min/max memory
     max_memory = info.max_memory
     min_memory = (max_memory * 0.25).to_i
-    # don't go below 1GB, or above max_memory
-    min_memory_floor = 1 * 1024 * 1024 * 1024
-    return if min_memory_floor > max_memory
+    return if @min_active > max_memory
 
-    min_memory = min_memory.clamp(1 * 1024 * 1024 * 1024, max_memory)
+    min_memory = min_memory.clamp(min_memory_floor, max_memory)
     new_active = mem_stat.actual * (memory_delta + 100) / 100
     new_active = new_active.clamp(min_memory..max_memory)
     return if new_active == mem_stat.actual
@@ -82,10 +88,10 @@ class BallooningVM
     @back_off_since = Time.now
   end
 
-  # @return [Boolean]
+  # @return [Boolean] true if we are backing off from issuing any further memory decrease commands.
   def backing_off?
     return false if @back_off_since.nil?
 
-    Time.now - @back_off_since >= 20
+    Time.now - @back_off_since >= @back_off_seconds
   end
 end
