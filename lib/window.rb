e0ec58f7f20ce2993b04e7e57c1d5ebe16500dba
Martin Vysny <mavi@vaadin.com>
2025-11-16 08:13:19 +0200

    fixes

--- lib/window.rb
+++ lib/window.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'tty-box'
 require 'tty-cursor'
 require 'pastel'
@@ -33,7 +35,7 @@ class Window
   # Sets new position of the window.
   # @param new_rect [Rect] new position
   def rect=(new_rect)
-    raise 'invalid rect #{new_rect}' unless new_rect.is_a? Rect
+    raise "invalid rect #{new_rect}" unless new_rect.is_a? Rect
 
     @rect = new_rect
     repaint
@@ -55,7 +57,7 @@ class Window
   end
 
   private def repaint
-    return if @rect.empty? || @rect.top < 0 || @rect.left < 0
+    return if @rect.empty? || @rect.top.negative? || @rect.left.negative?
 
     print TTY::Box.frame(
       width: @rect.width, height: @rect.height, top: @rect.top, left: @rect.left,
@@ -64,7 +66,9 @@ class Window
     repaint_content
   end
 
-  private def repaint_content
+  private
+
+  def repaint_content
     return if @rect.empty? || @rect.top < 0 || @rect.left < 0
 
     width = @rect.width - 4 # 1 character for window frame, 1 character for padding
