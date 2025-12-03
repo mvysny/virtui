01e8683b0313fc940d9937ca16c6bd29c031c6de
Martin Vysny <mavi@vaadin.com>
2025-12-03 13:49:55 +0200

    PickerWindow: more tests

--- spec/ttyui/picker_window_spec.rb
+++ spec/ttyui/picker_window_spec.rb
@@ -2,15 +2,43 @@
 
 require_relative '../spec_helper'
 require 'ttyui/picker_window'
+require 'ttyui/keys'
 
 describe PickerWindow do
-  before { Screen.new }
+  let(:screen) { Screen.fake }
   it 'smokes' do
     w = PickerWindow.new('foo', [PickerWindow::Option.new('a', 'all')]) {}
+    screen.add_window '1', w
     w.close
   end
   it 'doesnt call block if closed' do
     w = PickerWindow.new('foo', [PickerWindow::Option.new('a', 'all')]) { raise 'should not be called' }
+    screen.add_window '1', w
     w.handle_key('q')
+    assert !w.open?
+  end
+  it 'selects first option on enter' do
+    selected = nil
+    w = PickerWindow.new('foo', [PickerWindow::Option.new('a', 'all')]) { selected = it }
+    screen.add_window '1', w
+    w.handle_key(Keys::ENTER)
+    assert_equal 'a', selected
+    assert !w.open?
+  end
+  it 'selects correct option' do
+    selected = nil
+    w = PickerWindow.new('foo', [PickerWindow::Option.new('a', 'all')]) { selected = it }
+    screen.add_window '1', w
+    w.handle_key('a')
+    assert_equal 'a', selected
+    assert !w.open?
+  end
+  it 'does nothing if unlisted key is pressed' do
+    selected = nil
+    w = PickerWindow.new('foo', [PickerWindow::Option.new('a', 'all')]) { selected = it }
+    screen.add_window '1', w
+    w.handle_key('b')
+    assert_nil selected
+    assert w.open?
   end
 end
