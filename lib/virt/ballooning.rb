0c84a28d9f0afa041b4d26a7a82e3be1dba8ce4c
Martin Vysny <mavi@vaadin.com>
2025-12-17 16:13:21 +0200

    lower ballooning limits\

--- lib/virt/ballooning.rb
+++ lib/virt/ballooning.rb
@@ -82,13 +82,13 @@ class BallooningVM
     @boot_back_off_seconds = 20
 
     # When the guest mem usage (ommitting cache) is above this value, increase guest memory
-    @trigger_increase_at = 65
+    @trigger_increase_at = 60
 
     # When increasing memory, increase by how much
     @increase_memory_by = 30
 
     # When the guest mem usage (ommitting cache) is below this, start decreasing guest memory
-    @trigger_decrease_at = 55
+    @trigger_decrease_at = 50
 
     # When decreasing memory, decrease by how much
     @decrease_memory_by = 10
