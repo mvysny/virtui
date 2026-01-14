b891d9d1eea4b61a62dcb8b72abb0d690caa711a
Martin Vysny <mavi@vaadin.com>
2026-01-14 09:04:10 +0200

    fix tests

--- spec/vm_window_spec.rb
+++ spec/vm_window_spec.rb
@@ -9,11 +9,13 @@ require 'timecop'
 describe VMWindow do
   let(:now) { Time.now }
   let(:window) do
+    Screen.fake
     cache = Timecop.freeze(now) { VirtCache.new(VMEmulator.demo, PcEmulator.new) }
     w = Timecop.freeze(now + 5) do
       cache.update
       VMWindow.new(cache, Ballooning.new(cache))
     end
+    w.rect = Rect.new(0, 0, 20, 20)
     w.active = true
     w.show_disk_stat = true
     w
@@ -21,15 +23,15 @@ describe VMWindow do
 
   it 'has the right content' do
     content = window.content.map { Rainbow.uncolor(it) }
-    assert_equal '⏹ BASE', content[0]
+    assert_equal '⏹ BASE──────────', content[0]
     assert_equal '    vda: 50%   64G   128G | ', content[1]
-    assert_equal '⏹ Fedora', content[2]
+    assert_equal '⏹ Fedora────────', content[2]
     assert_equal '    vda: 50%   64G   128G | ', content[3]
-    assert_equal '▶ Ubuntu 🎈', content[4]
+    assert_equal '▶ Ubuntu 🎈─────', content[4]
     assert_equal '    CPU:  0%          1 t |   0%          8 t', content[5]
     assert_equal '    RAM: 25%    2G   7.9G |   9%  3.1G    32G', content[6]
     assert_equal '    vda: 50%   64G   128G | ', content[7]
-    assert_equal '▶ win11 🎈', content[8]
+    assert_equal '▶ win11 🎈──────', content[8]
     assert_equal '    CPU:  0%          1 t |   0%          8 t', content[9]
     assert_equal '    RAM: 25%    2G   7.9G |   9%  3.1G    32G', content[10]
     assert_equal '    vda: 50%   64G   128G | ', content[11]
@@ -39,28 +41,28 @@ describe VMWindow do
     it 'moves cursor down correctly' do
       assert_equal 0, window.cursor.position
       # first VM is stopped and takes 2 lines
-      window.handle_key("\e[B")
+      window.handle_key(Keys::DOWN_ARROW)
       assert_equal 2, window.cursor.position
       # second VM is running and takes 3 lines
-      window.handle_key("\e[B")
+      window.handle_key(Keys::DOWN_ARROW)
       assert_equal 4, window.cursor.position
       # third VM is running and takes 3 lines
-      window.handle_key("\e[B")
+      window.handle_key(Keys::DOWN_ARROW)
       assert_equal 8, window.cursor.position
       # no more VMs
-      window.handle_key("\e[B")
+      window.handle_key(Keys::DOWN_ARROW)
       assert_equal 8, window.cursor.position
     end
     it 'moves cursor up correctly' do
       window.cursor.go(8)
       assert_equal 8, window.cursor.position
-      window.handle_key("\e[A")
+      window.handle_key(Keys::UP_ARROW)
       assert_equal 4, window.cursor.position
-      window.handle_key("\e[A")
+      window.handle_key(Keys::UP_ARROW)
       assert_equal 2, window.cursor.position
-      window.handle_key("\e[A")
+      window.handle_key(Keys::UP_ARROW)
       assert_equal 0, window.cursor.position
-      window.handle_key("\e[A")
+      window.handle_key(Keys::UP_ARROW)
       assert_equal 0, window.cursor.position
     end
   end
