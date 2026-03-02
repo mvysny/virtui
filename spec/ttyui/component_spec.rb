648201a698682901c49632c9e5caa634dd57ed8c
Martin Vysny <mavi@vaadin.com>
2026-03-02 09:10:38 +0200

    test: add tests for Component and Label

--- spec/ttyui/component_spec.rb
+++ spec/ttyui/component_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../spec_helper'
-require 'ttyui/component'
-require 'tty-logger'
+require 'ttyui/screen'
+require 'strings-truncation'
 
 describe Rect do
   describe '#at' do
@@ -124,34 +124,193 @@ end
 describe Component do
   before { Screen.fake }
   after { Screen.close }
+
   it 'smokes' do
     Component.new
+  end
+
+  context 'rect=' do
+    it 'raises on non-Rect argument' do
+      assert_raises(RuntimeError) { Component.new.rect = 'not a rect' }
+    end
+
+    it 'is no-op when set to the same rect' do
+      c = Component.new
+      c.rect = Rect.new(0, 0, 10, 5)
+      Screen.instance.invalidated_clear
+      c.rect = Rect.new(0, 0, 10, 5)
+      assert !Screen.instance.invalidated?(c)
+    end
+
+    it 'invalidates when rect changes' do
+      c = Component.new
+      c.rect = Rect.new(0, 0, 10, 5)
+      assert Screen.instance.invalidated?(c)
+    end
+
+    it 'calls on_width_changed when width changes' do
+      width_changed = false
+      klass = Class.new(Component) { define_method(:on_width_changed) { width_changed = true } }
+      c = klass.new
+      c.rect = Rect.new(0, 0, 20, 5)
+      assert width_changed
+    end
+
+    it 'does not call on_width_changed when only height changes' do
+      width_changed = false
+      klass = Class.new(Component) { define_method(:on_width_changed) { width_changed = true } }
+      c = klass.new
+      c.rect = Rect.new(0, 0, 10, 5)
+      width_changed = false
+      c.rect = Rect.new(0, 0, 10, 10)
+      assert !width_changed
+    end
+  end
+
+  context 'active' do
+    it 'is false by default' do
+      assert !Component.new.active?
+    end
+
+    it 'raises when trying to activate a non-activatable component' do
+      assert_raises(RuntimeError) { Component.new.active = true }
+    end
+
+    it 'setting false when already false is a no-op' do
+      c = Component.new
+      assert !Screen.instance.invalidated?(c)
+      c.active = false
+      assert !Screen.instance.invalidated?(c)
+    end
+  end
+
+  it 'can_activate? is false by default' do
+    assert !Component.new.can_activate?
+  end
+
+  it 'handle_key returns false' do
+    assert_equal false, Component.new.handle_key('a')
+  end
+
+  it 'handle_mouse returns nil' do
+    assert_nil Component.new.handle_mouse(nil)
+  end
+
+  context 'clear_background' do
+    it 'skips when rect is empty' do
+      c = Component.new
+      c.send(:clear_background)
+      assert_equal [], Screen.instance.prints
+    end
+
+    it 'prints spaces for each row of the rect' do
+      c = Component.new
+      c.rect = Rect.new(2, 3, 5, 2)
+      Screen.instance.prints.clear
+      c.send(:clear_background)
+      assert_equal [TTY::Cursor.move_to(2, 3), '     ',
+                    TTY::Cursor.move_to(2, 4), '     '], Screen.instance.prints
+    end
+  end
+
+  it 'invalidate adds component to screen invalidated set' do
+    c = Component.new
+    Screen.instance.invalidated_clear
+    c.send(:invalidate)
+    assert Screen.instance.invalidated?(c)
   end
 end
 
 describe Component::Label do
   before { Screen.fake }
   after { Screen.close }
+
   it 'smokes' do
     label = Component::Label.new
     label.text = 'Test 1 2 3 4'
   end
+
   it 'can repaint on unset text' do
     label = Component::Label.new
     label.repaint
     assert_equal [], Screen.instance.prints
   end
-  it 'can repaint on unset text' do
+
+  it 'clears background when text is empty' do
     label = Component::Label.new
     label.rect = Rect.new(0, 0, 5, 1)
     label.repaint
     assert_equal ["\e[1;1H", '     '], Screen.instance.prints
   end
-  it 'prints only first line' do
+
+  it 'prints only first line when height is 1' do
     label = Component::Label.new
     label.rect = Rect.new(0, 0, 5, 1)
     label.text = "1\n2\n3"
     label.repaint
     assert_equal ["\e[1;1H", '     ', "\e[1;1H", '1'], Screen.instance.prints
+  end
+
+  it 'prints multiple lines within rect height' do
+    label = Component::Label.new
+    label.rect = Rect.new(0, 0, 10, 3)
+    label.text = "foo\nbar\nbaz"
+    label.repaint
+    assert_equal [TTY::Cursor.move_to(0, 0), '          ',
+                  TTY::Cursor.move_to(0, 1), '          ',
+                  TTY::Cursor.move_to(0, 2), '          ',
+                  TTY::Cursor.move_to(0, 0), 'foo',
+                  TTY::Cursor.move_to(0, 1), 'bar',
+                  TTY::Cursor.move_to(0, 2), 'baz'], Screen.instance.prints
+  end
+
+  it 'clips lines vertically when text has more lines than height' do
+    label = Component::Label.new
+    label.rect = Rect.new(0, 0, 10, 2)
+    label.text = "one\ntwo\nthree"
+    label.repaint
+    assert_equal [TTY::Cursor.move_to(0, 0), '          ',
+                  TTY::Cursor.move_to(0, 1), '          ',
+                  TTY::Cursor.move_to(0, 0), 'one',
+                  TTY::Cursor.move_to(0, 1), 'two'], Screen.instance.prints
+  end
+
+  it 'truncates lines longer than rect width' do
+    label = Component::Label.new
+    label.rect = Rect.new(0, 0, 5, 1)
+    label.text = 'hello world'
+    label.repaint
+    truncated = Strings::Truncation.truncate('hello world', length: 5)
+    assert_equal [TTY::Cursor.move_to(0, 0), '     ',
+                  TTY::Cursor.move_to(0, 0), truncated], Screen.instance.prints
+  end
+
+  it 'handles nil text gracefully' do
+    label = Component::Label.new
+    label.rect = Rect.new(0, 0, 5, 1)
+    label.text = nil
+    label.repaint
+    assert_equal [TTY::Cursor.move_to(0, 0), '     '], Screen.instance.prints
+  end
+
+  it 're-clips text when width changes' do
+    label = Component::Label.new
+    label.rect = Rect.new(0, 0, 3, 1)
+    label.text = 'hello world'
+    label.rect = Rect.new(0, 0, 5, 1)
+    label.repaint
+    assert_equal [TTY::Cursor.move_to(0, 0), '     ',
+                  TTY::Cursor.move_to(0, 0), Strings::Truncation.truncate('hello world', length: 5)],
+                 Screen.instance.prints
+  end
+
+  it 'does not invalidate when text is set to the same value again' do
+    label = Component::Label.new
+    label.rect = Rect.new(0, 0, 5, 1)
+    label.text = 'hi'
+    invalidated = Screen.instance.instance_variable_get(:@invalidated)
+    invalidated.clear
+    label.text = 'hi'
+    assert !invalidated.include?(label)
   end
 end
