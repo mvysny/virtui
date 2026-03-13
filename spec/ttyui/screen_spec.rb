2e0cd66b530e3baf41319298e6cbdb7da10db678
Martin Vysny <mavi@vaadin.com>
2026-03-13 18:24:13 +0200

    more tests

--- spec/ttyui/screen_spec.rb
+++ spec/ttyui/screen_spec.rb
@@ -66,6 +66,45 @@ describe Screen do
       assert_nil screen2.content
       screen2.focused = nil
     end
+
+    it 'marks all ancestor layouts active when focusing a nested window' do
+      nested_layout = Component::Layout::Absolute.new
+      screen.content.add(nested_layout)
+      w = Window.new
+      nested_layout.add(w)
+      screen.focused = w
+      assert w.active?
+      assert nested_layout.active?
+    end
+
+    it 'deactivates ancestor layouts when focus moves to a different branch' do
+      layout1 = Component::Layout::Absolute.new
+      layout2 = Component::Layout::Absolute.new
+      screen.content.add(layout1)
+      screen.content.add(layout2)
+      w1 = Window.new
+      w2 = Window.new
+      layout1.add(w1)
+      layout2.add(w2)
+      screen.focused = w1
+      screen.focused = w2
+      assert !w1.active?
+      assert !layout1.active?
+      assert w2.active?
+      assert layout2.active?
+    end
+
+    it 'propagates handle_key through nested layouts to focused window' do
+      nested_layout = Component::Layout::Absolute.new
+      screen.content.add(nested_layout)
+      w = Window.new
+      nested_layout.add(w)
+      screen.focused = w
+      handled = false
+      w.define_singleton_method(:handle_key) { |_key| handled = true }
+      screen.content.handle_key('x')
+      assert handled
+    end
   end
 
   context 'active_window' do
